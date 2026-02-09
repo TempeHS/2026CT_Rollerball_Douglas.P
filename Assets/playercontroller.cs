66050749340d8ed56323a4c9eec3de1eb6b7ec3d
Douglas P <douglas.percival@education.nsw.gov.au>
2026-02-09 11:53:48 +1100

    WIP player controller

---
+++ Assets/playercontroller.cs
@@ -0,0 +1,16 @@
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playercontroller: MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Onmove()
+    {
+        
+    }
+}
