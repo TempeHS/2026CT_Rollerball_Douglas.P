95e7a5b4ba43250aa9fd2dbd6e9df409630824de
TAD 11 <gspacewhale23@gmail.com>
2026-05-01 11:44:45 +1000

    Created enemy distance tracker

--- Assets/Scripts/Velociy tracker.cs
+++ Assets/Scripts/Velociy tracker.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class Velociytracker : MonoBehaviour
 {
     public TextMeshProUGUI velocityText;
+    public TextMeshProUGUI enemyDistance;
     public Player_Controller countTracker;
     public int countRequired = 13;
     public GameObject VelocityTextObject;
+    public GameObject EnemyDistanceObject;
+    public Transform Enemy;
+
 
     private Vector3 lastPosition;
     private float distanceTraveled;
     private float averageVelocity;
+    private float EnemyDistance;
    
     private bool hasCalculated = false;
 
@@ -20,11 +25,13 @@ public class Velociytracker : MonoBehaviour
     {
         lastPosition = transform.position;
         VelocityTextObject.SetActive(false);
+        EnemyDistanceObject.SetActive(false);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        EnemyDistance = Vector3.Distance(Enemy.position,transform.position );
         distanceTraveled += Vector3.Distance(transform.position, lastPosition);
         lastPosition = transform.position;
         if(!hasCalculated && countTracker.count >= countRequired)
@@ -36,6 +43,7 @@ public class Velociytracker : MonoBehaviour
             VelocityTextObject.SetActive(true);
             velocityText.text = "average velocity: " + averageVelocity.ToString () + " M/S";
         }
+        Debug.Log(EnemyDistance);
     }
     void OnCollisionEnter(Collision collision)
     {
