50207971440e96e22d5f1bc05ec53a62669a76a1
TAD 11 <gspacewhale23@gmail.com>
2026-05-12 13:58:47 +1000

    wawaaw

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -7,6 +7,7 @@ public class EnemyMovement : MonoBehaviour
     public Transform player;
     private NavMeshAgent navMeshAgent;
 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
